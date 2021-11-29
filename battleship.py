99cd13aeae4bc9c371ac39af5de93fae963f6b5b
yetsy munñoz córdoba <yemc0995@outlook.com>
2021-11-28 21:45:26 -0600

    cambios

--- battleship.py
+++ battleship.py
@@ -15,23 +15,25 @@ while True:
             l=l+[1,2,3,4,5]
             random.shuffle(l)
             return (l)
+        barco = inicializarBarcosComputadora()
     
         
         
         def inializarBarcosJugador():
             l=[0]*20
             for i in range(1,6):
-                posic=int(input("Deme una posicion para el barco con num: "+str(i)))
+                posic=int(input("Deme una posicion para el barco número "+str(i)+": "))
                 l[posic]=i
             return l
+        barco2 = inializarBarcosJugador()
         print ("POSICIONES CREADAS")
         
 
     elif opc == '2':
         print ("Posicion de barcos de la computadora: ")
-        print (inicializarBarcosComputadora())
+        print (barco)
         print ("Posicion de barcos del Jugador: ")
-        print(inializarBarcosJugador())
+        print(barco2)
     #elif opc == '3':
         #posicionjugador =int(input("JUGADOR: Ingresa la posicion (de 1 a 20) a donde quieres disparar: "))
         #for i in range(len (inicializarBarcosComputadora())):
@@ -43,21 +45,19 @@ while True:
 
     #PRUEBA
     elif opc == '3':
-        print ("CUSTODIAR" ,inicializarBarcosComputadora())
-        def hacerDisparo(inicializarBarcosComputadora):
+        
+        def hacerDisparo(barco):
 
-            print ("CUSTODIAR" ,inicializarBarcosComputadora())
-            posicionjugador =int(input("JUGADOR: Ingresa la posicion (de 1 a 20) a donde quieres disparar: "))
-            for i in range(len (inicializarBarcosComputadora())):
-                if posicionjugador in inicializarBarcosComputadora():
-                   inicializarBarcosComputadora()[i] = 0
-                   print ("TOMELA" ,inicializarBarcosComputadora()) 
+            posicionjugador = int(input("JUGADOR: Ingresa la posicion (de 1 a 20) a donde quieres disparar: "))
+            for i in range(len (barco)):
+                if posicionjugador in barco:
+                   barco[i] = 0
+                   return print ("TOMELA" ,barco) 
                 else:
-                    print("MAMASTE")
-            print ("CUSTODIAR" ,inicializarBarcosComputadora())
+                    return print("MAMASTE")
 
-        hacerDisparo(inicializarBarcosComputadora)
-        break    
+        hacerDisparo(barco)
+
     elif opc == '4':
         print("bais")
         
